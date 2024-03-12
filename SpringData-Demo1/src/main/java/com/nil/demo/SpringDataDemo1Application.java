a47d5c1c2a627191c88684425de71f468a79dc30
Nilesh-IT <developermicroservices@gmail.com>
2024-03-13 01:15:03 +0530

    first coommit

---
+++ SpringData-Demo1/src/main/java/com/nil/demo/SpringDataDemo1Application.java
@@ -0,0 +1,13 @@
+package com.nil.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataDemo1Application.class, args);
+	}
+
+}
---
+++ SpringData-Demo1/src/test/java/com/nil/demo/SpringDataDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nil.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
